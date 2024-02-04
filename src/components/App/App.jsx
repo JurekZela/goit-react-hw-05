fd0c0570ceba97a4089499b8b40e923bfd30b696
JurekZela <122160360+JurekZela@users.noreply.github.com>
2024-02-04 15:44:54 +0100

    add vercel.json

---
+++ src/components/App/App.jsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+// import {Routes, Route } from 'react-router-dom';
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p>Hello World</p>
+      <h1>This is my project Kino</h1>
+    </>
+  )
+}
+
+export default App
